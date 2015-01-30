4433695cfb20970f247ba2bf04b473d23117ecd9
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-01-30 12:02:09 +0000

    Added basic homepage and basic login page with route. Additionally added the bootstrap framework.

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Brella' });
+  res.render('login', { title: 'Brella' });
 });
 
 module.exports = router;
