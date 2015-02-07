ba4e82111db42ac22e1a6782c4f98ccd7d84bb94
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-02-07 20:12:30 +0000

    Index page now allows searching of cities. Results displayed as a group list on the index page (for the time being).

--- routes/findCity.js
+++ routes/findCity.js
@@ -4,21 +4,25 @@ var City = require('../models/City');
 
 /* GET findCity. */
 router.get('/:cityToFind', function(req, res) {
-    //TODO: Execute search only after 3 or more chars entererd?
     var cityToFind = req.params.cityToFind;
-    console.log("Searching for cities containing: " + cityToFind + "...");
-    //TODO: Check the regex
-    City.find({cityName: { $regex: new RegExp("^" + cityToFind.toLowerCase(), "i") }}, function(err, doc) {
-        if (err) return console.error(err);
-        if (doc) {
-            var foundCities = doc;
-            //TODO: Strip out db id and __v items
-            console.log("...found " + doc.length + " cities matching " + cityToFind);
-            res.json(foundCities);
-        } else {
-            res.json({error : 'City: ' + cityToFind + ' not found'});
-        }
-    });
+    //Execute search only after 3 or more chars entererd
+    if (cityToFind.length >= 3) {
+        console.log("Searching for cities containing: " + cityToFind + "...");
+        //TODO: Check the regex. Needs to allow "City of London" when searching for "london"
+        City.find({cityName: { $regex: new RegExp("" + cityToFind.toLowerCase(), "i") }}, function(err, doc) {
+            if (err) return console.error(err);
+            if (doc) {
+                var foundCities = doc;
+                //TODO: Strip out db id and __v items
+                console.log("...found " + doc.length + " cities matching " + cityToFind);
+                res.json(foundCities);
+            } else {
+                res.json({error : 'City: ' + cityToFind + ' not found'});
+            }
+        });
+    } else {
+        res.json({error : 'City query not long enough, need 3 characters or more'});
+    }
 });
 
 module.exports = router;
