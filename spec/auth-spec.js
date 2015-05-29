6556bbf2492edeb9f1faa9e15d8e0e8565946389
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-05-29 17:35:41 +0100

    Added basic tests for the authentication library

---
+++ spec/auth-spec.js
@@ -0,0 +1,31 @@
+var auth = require('../auth');
+
+//To test the Authentication library that uses bcrypt
+describe("Authentication library", function() {
+
+    var testPassword = "P@ssw0rd1234";
+    var testHashPassword = null;    //The generatePassword call will create this hash
+
+    it("should generate a hashed password from plaintext", function(done) {
+        auth.generatePassword(testPassword, function(password) {
+            testHashPassword = password;
+            expect(password).not.toBe(testPassword);
+            done();
+        });
+    });
+
+    it("should compare a hashed password with a plaintext", function(done) {
+        auth.checkPassword(testPassword, testHashPassword, function(res) {
+            expect(res).toBe(true);
+            done();
+        });
+    });
+
+    it("should reject an incorrect hashed password", function(done) {
+        auth.checkPassword(testPassword, "rubbishHash", function(res) {
+            expect(res).toBe(false);
+            done();
+        });
+    });
+
+});
