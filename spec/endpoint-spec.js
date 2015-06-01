c660cd35c827590319ab1cd06fee977328fa532e
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-06-01 18:32:23 +0100

    Created tests for the getWeather API endpoint

--- spec/endpoint-spec.js
+++ spec/endpoint-spec.js
@@ -13,7 +13,7 @@ describe("findCity API endpoint", function() {
         .expect(404, done);
     });
 
-    it("should return error JSON when less than three characters are entered", function(done) {
+    it("should return JSON error when less than three characters are entered", function(done) {
         supertest
         .get('/findCity/ha')
         .set('Accept', 'application/json')
@@ -24,7 +24,6 @@ describe("findCity API endpoint", function() {
                 "City query not long enough, need 3 characters or more");
             done();
         });
-
     });
 
     it('should return empty array for non-existant city', function(done){
@@ -51,5 +50,61 @@ describe("findCity API endpoint", function() {
             done();
         });
     });
+
+});
+
+describe("getWeather API endpoint", function() {
+
+    it('should return a 404 error when no cityID is entered', function(done) {
+        supertest
+        .get('/getWeather/')
+        .set('Accept', 'application/json')
+        .expect(404, done);
+    });
+
+    it("should return JSON error when characters are entered as a cityID", function(done) {
+        var characterCityID = "abcdefg";
+        supertest
+        .get('/getWeather/' + characterCityID)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+            expect(res.status).toBe(404);
+            expect(res.type).toBe('application/json');
+            expect(res.body.error).toEqual(
+                "The city ID: " + characterCityID + " is not a number");
+            done();
+        });
+    });
+
+    it("should return JSON error when alpha-numeric " +
+    "characters are entered as a cityID", function(done) {
+        var characterCityID = "abcdefg123456";
+        supertest
+        .get('/getWeather/' + characterCityID)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+            expect(res.status).toBe(404);
+            expect(res.type).toBe('application/json');
+            expect(res.body.error).toEqual(
+                "The city ID: " + characterCityID + " is not a number");
+            done();
+        });
+    });
+
+    it("should return JSON error when a non-existant cityID is entered", function(done) {
+        supertest
+        .get('/getWeather/123456')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+            expect(res.status).toBe(404);
+            expect(res.type).toBe('application/json');
+            expect(res.body.error.message).toEqual("Error: Not found city");
+            expect(res.body.error.cod).toEqual("404");
+            done();
+        });
+    }, 8000);   //We give it a bit longer as its accessing HTTP API
 
 });
