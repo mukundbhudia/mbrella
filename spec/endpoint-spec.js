4a14e2b9708d893f7ba9873cf558255fab69ea4d
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-06-01 12:18:17 +0100

    Added more tests for findCity endpoint

--- spec/endpoint-spec.js
+++ spec/endpoint-spec.js
@@ -6,6 +6,27 @@ var supertest = require('supertest')(app);
 
 describe("findCity API endpoint", function() {
 
+    it("should return a 404 error when no city is entered", function(done) {
+        supertest
+        .get('/findCity/')
+        .set('Accept', 'application/json')
+        .expect(404, done);
+    });
+
+    it("should return error JSON when less than three characters are entered", function(done) {
+        supertest
+        .get('/findCity/ha')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+            expect(res.body.error).toEqual(
+                "City query not long enough, need 3 characters or more");
+            done();
+        });
+
+    });
+
     it('should return empty array for non-existant city', function(done){
         supertest
         .get('/findCity/testfakecity')
