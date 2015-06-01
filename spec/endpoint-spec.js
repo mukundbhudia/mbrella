cace6cf00226b6aa0f28c6712b8af4f9ad8efad2
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-06-01 21:47:54 +0100

    Fixed content type and HTTP status expectations in tests

--- spec/endpoint-spec.js
+++ spec/endpoint-spec.js
@@ -2,24 +2,21 @@ var app = require('../app');
 
 var request = require('http');
 var supertest = require('supertest')(app);
-// var api = supertest('http://localhost:3000');
 
 describe("findCity API endpoint", function() {
 
     it("should return a 404 error when no city is entered", function(done) {
         supertest
         .get('/findCity/')
-        .set('Accept', 'application/json')
         .expect(404, done);
     });
 
     it("should return JSON error when less than three characters are entered", function(done) {
         supertest
         .get('/findCity/ha')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
         .end(function(err, res) {
+            expect(res.status).toBe(200);
+            expect(res.type).toBe('application/json');
             expect(res.body.error).toEqual(
                 "City query not long enough, need 3 characters or more");
             done();
@@ -29,10 +26,9 @@ describe("findCity API endpoint", function() {
     it('should return empty array for non-existant city', function(done){
         supertest
         .get('/findCity/testfakecity')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
         .end(function(err, res) {
+            expect(res.status).toBe(200);
+            expect(res.type).toBe('application/json');
             expect(res.body).toEqual([]);
             done();
         });
@@ -41,10 +37,9 @@ describe("findCity API endpoint", function() {
     it('should return one city named: Dubai for the entered text: dubai', function(done){
         supertest
         .get('/findCity/dubai')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
         .end(function(err, res) {
+            expect(res.status).toBe(200);
+            expect(res.type).toBe('application/json');
             expect(res.body.length).toBe(1);
             expect(res.body[0].cityName).toEqual("Dubai");
             done();
@@ -58,7 +53,6 @@ describe("getWeather API endpoint", function() {
     it('should return a 404 error when no cityID is entered', function(done) {
         supertest
         .get('/getWeather/')
-        .set('Accept', 'application/json')
         .expect(404, done);
     });
 
@@ -66,8 +60,6 @@ describe("getWeather API endpoint", function() {
         var characterCityID = "abcdefg";
         supertest
         .get('/getWeather/' + characterCityID)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
         .end(function(err, res) {
             expect(res.status).toBe(404);
             expect(res.type).toBe('application/json');
@@ -82,8 +74,6 @@ describe("getWeather API endpoint", function() {
         var characterCityID = "abcdefg123456";
         supertest
         .get('/getWeather/' + characterCityID)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
         .end(function(err, res) {
             expect(res.status).toBe(404);
             expect(res.type).toBe('application/json');
@@ -96,8 +86,6 @@ describe("getWeather API endpoint", function() {
     it("should return JSON error when a non-existant cityID is entered", function(done) {
         supertest
         .get('/getWeather/123456')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
         .end(function(err, res) {
             expect(res.status).toBe(404);
             expect(res.type).toBe('application/json');
